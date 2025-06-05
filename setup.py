3c729cf6cc6b44eb21fc67e4a8bf6f4fd48dc161
CyprienBouton <boutoncyprien@yahoo.com>
2025-06-05 15:35:45 +0200

    [fix] setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(name='recotwix', # this will be name of package in packages list : pip lis
             'torch',
             'twixtools', 
             'scipy',
-            'ggrappa@git+https://github.com/username/repo-name@branch-or-tag'
+            'ggrappa @ git+https://github.com/mind-inria/ggrappa.git@main',
       ]
      )
